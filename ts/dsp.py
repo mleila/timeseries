4f7fc6e99365d71bf2ff3710ec8caea1387b9b4c
mleila <mohamed.laila@mail.mcgill.ca>
2020-10-03 14:13:14 -0400

    [NEW] add dft_report function to dsp module

--- ts/dsp.py
+++ ts/dsp.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 '''This module provides functions related to Digital Signal Processing and Fourier Analysis.'''
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def make_sinusoidal_signal(
@@ -25,3 +26,23 @@ def make_sinusoidal_signal(
     total_length = points * freq
     noise = np.random.normal(noise_mean, noise_var, size=total_length)
     return full_signal + noise
+
+
+def dft_report(signal):
+    # compute dft
+    fft = np.fft.fft(signal)
+
+    # get real components (index is freq as k/N of 2pi, value is the energy)
+    real = np.abs(np.real(fft))
+
+    # keep only the first half
+    midpoint = int(signal.size / 2)
+    real = real[:midpoint] / signal.size
+
+    top_freq_index = np.argmax(real[1:]) + 1
+    top_freq_amp = real[top_freq_index] * 2
+    print(f'Average = {real[0]:.2f}', )
+    print(f'Index of most prominent freq = {top_freq_index}')
+    print(f'Top Freq Amplitude = {top_freq_amp:.4f}')
+    print(f'''Period of the most prominent freq = {int(signal.size/top_freq_index)}''')
+    plt.stem(2*real[1:]);
