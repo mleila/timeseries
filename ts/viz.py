f2f777bcb4b37c3d2db1e7835dc1e8a464d7321f
mleila <mohamed.laila@mail.mcgill.ca>
2020-10-04 20:16:47 -0400

    [NEW] add index to plot_basic_timeseries function

--- ts/viz.py
+++ ts/viz.py
@@ -3,6 +3,7 @@ from pkg_resources import resource_filename
 from typing import Union, Optional
 
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 
@@ -20,6 +21,7 @@ def plot_basic_timeseries(
     timeseries: Union[list, np.array],
     name: str=None,
     names: list=[],
+    index: Union[pd.Series, list]=None
     )-> go.Figure:
 
     fig = go.Figure()
@@ -27,13 +29,13 @@ def plot_basic_timeseries(
         title = f'Timeseries Data'
         names = names if names else [f'ts{i}' for i in range(len(timeseries))]
         for ts, name in zip(timeseries, names):
-            scatter = go.Scatter(y=ts, mode='lines', name=name)
+            scatter = go.Scatter(x=index, y=ts, mode='lines', name=name)
             fig.add_trace(scatter)
         fig.update_layout(title=title)
         return fig
 
     title = f'{name}' if name else ''
-    scatter = go.Scatter(y=timeseries, mode='lines')
+    scatter = go.Scatter(x=index, y=timeseries, mode='lines')
     fig.add_trace(scatter)
     fig.update_layout(title=title)
     return fig
